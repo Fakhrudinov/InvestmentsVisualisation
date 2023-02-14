010248c9015584a3ec4af2e4b3234e8622b7c434
asbuka1975 <asbuka@gmail.com>
2023-02-14 20:44:56 +0300

    Add initial asp mvc

---
+++ InvestmentVisualisation/InvestmentVisualisation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InvestmentVisualisation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
