2b0d2b62b9d63170786092e24540fdf30e519346
asbuka1975 <asbuka@gmail.com>
2023-02-23 12:26:00 +0300

    secVolume signs after commf

--- InvestmentVisualisation/DataAbstraction/Models/SecVolume/SecVolumeModel.cs
+++ InvestmentVisualisation/DataAbstraction/Models/SecVolume/SecVolumeModel.cs
@@ -1,11 +1,16 @@
 ﻿using DataAbstraction.Models.BaseModels;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAbstraction.Models.SecVolume
 {
     public class SecVolumeModel : SecCodeAndSecBoardModel
     {
         public int Pieces { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public decimal AvPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public decimal Volume { get; set; }
     }
 }
