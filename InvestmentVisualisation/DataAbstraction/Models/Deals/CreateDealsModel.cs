e62156cee656fde4c9d8c1c8b59cec293eb9300d
asbuka1975 <asbuka@gmail.com>
2023-02-23 08:59:59 +0300

    fix remove incorrect validation

--- InvestmentVisualisation/DataAbstraction/Models/Deals/CreateDealsModel.cs
+++ InvestmentVisualisation/DataAbstraction/Models/Deals/CreateDealsModel.cs
@@ -6,7 +6,6 @@ namespace DataAbstraction.Models.Deals
     public class CreateDealsModel : BaseModel
     {
         [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "The value must be greater than 0")]
         [Display(Name = "Средняя цена")] 
         [RegularExpression("^-?(0|[1-9]\\d{0,7})([.|,]\\d{1,6})?$", ErrorMessage = "Только цифры. Разделитель - точка или запятая.")]
         public string AvPrice { get; set; }
@@ -16,7 +15,6 @@ namespace DataAbstraction.Models.Deals
         [Display(Name = "Количество")]
         public int Pieces { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "The value must be greater than 0")]
         [Display(Name = "НКД")]
         [RegularExpression("^-?(0|[1-9]\\d{0,7})([.|,]\\d{1,6})?$", ErrorMessage = "Только цифры. Разделитель - точка или запятая.")]
         public string? NKD { get; set; }
--- InvestmentVisualisation/DataAbstraction/Models/Incoming/IncomingModel.cs
+++ InvestmentVisualisation/DataAbstraction/Models/Incoming/IncomingModel.cs
@@ -11,7 +11,6 @@ namespace DataAbstraction.Models.Incoming
         public int Category { get; set; }
 
         [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "The value must be greater than 0")]
         [Display(Name = "Сумма")]
         [RegularExpression("^-?(0|[1-9]\\d{0,7})([.|,]\\d{1,6})?$", ErrorMessage = "Только цифры. Разделитель - точка или запятая.")]
         public string Value { get; set; }
