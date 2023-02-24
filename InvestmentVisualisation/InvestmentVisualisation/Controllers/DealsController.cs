a4feb68ce42805add8cde946f546e7408b64dbb4
asbuka1975 <asbuka@gmail.com>
2023-02-24 14:14:04 +0300

    create deals from brok report string

--- InvestmentVisualisation/InvestmentVisualisation/Controllers/DealsController.cs
+++ InvestmentVisualisation/InvestmentVisualisation/Controllers/DealsController.cs
@@ -12,12 +12,18 @@ namespace InvestmentVisualisation.Controllers
         private readonly ILogger<DealsController> _logger;
         private IMySqlDealsRepository _repository;
         private int _itemsAtPage;
+        private IMySqlSecCodesRepository _secCodesRepo;
 
-        public DealsController(ILogger<DealsController> logger, IMySqlDealsRepository repository, IOptions<PaginationSettings> paginationSettings)
+        public DealsController(
+            ILogger<DealsController> logger, 
+            IMySqlDealsRepository repository, 
+            IOptions<PaginationSettings> paginationSettings,
+            IMySqlSecCodesRepository secCodesRepo)
         {
             _logger = logger;
             _repository = repository;
             _itemsAtPage = paginationSettings.Value.PageItemsCount;
+            _secCodesRepo = secCodesRepo;
         }
 
 
@@ -61,6 +67,97 @@ namespace InvestmentVisualisation.Controllers
             model.Pieces = pieces;
             return View("Create", model);
         }
+        [HttpPost]
+        public async Task<IActionResult> CreateFromText(string text)
+        {
+            _logger.LogInformation($"{DateTime.Now.ToString("HH:mm:ss:fffff")} DealsController HttpPost CreateFromText called, text={text}");
+
+            //"7318187850\t34614262505\tПокупка ЦБ на бирже\t15.02.2023 10:40:43\tБанк СПб, ПАО ао03, 10300436B\tRU0009100945\t1.00\t50.00\t112.07\tRUB\t5,603.50\t0.00\tRUB\t0.00\t0.95\t0.00\t17.02.2023\t17.02.2023\tПАО Московская Биржа"
+            //"Покупка ЦБ на бирже\t15.02.2023 10:53:59\tСелигдар, ПАО ао01, 1-01-32694-F\tRU000A0JPR50\t1.00\t100.00\t46.83\tRUB\t4,683.00\t0.00\tRUB\t0.00\t0.80\t0.0"
+
+            if (text is null || !text.Contains("\t"))
+            {
+                ViewData["Message"] = "Чтение строки не удалось, строка пустая или не содержит табуляций-разделителей: " + text;
+                return View("Create");
+            }
+
+            string[] textSplitted = text.Split("\t");
+            if (textSplitted.Length < 13)
+            {
+                ViewData["Message"] = "Чтение строки не удалось, получено менее 13 элементов (12х табуляций-разделителей) в строке: " + text;
+                return View("Create");
+            }
+
+            // поищем, где расположен текст "Покупка ЦБ на бирже" - от этой точки будем считывать столбцы
+            int startPointer = 0;
+            for (int i = 0; i < textSplitted.Length; i++)
+            {
+                if (textSplitted[i].Contains("Покупка ЦБ на бирже"))
+                {
+                    startPointer = i;
+                    break;
+                }
+
+                if (i > 5)
+                {
+                    ViewData["Message"] = "Не найдена точка входа 'Покупка ЦБ на бирже' в тексте: " + text;
+                    return View("Create");
+                }
+            }
+
+            // начинаем заполнение
+            CreateDealsModel model = new CreateDealsModel();
+
+            // дата // 15.02.2023 10:40:43
+            //string rawData = textSplitted[startPointer + 1];
+            //string[] dataSplitted = textSplitted[startPointer + 1].Split(' ').First().Split('.');
+            //string dateString = $"{dataSplitted[2]}-{dataSplitted[1]}-{dataSplitted[0]} {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}";
+            //model.Date = DateTime.Parse(dateString);
+            model.Date = DateTime.Parse(textSplitted[startPointer + 1]);
+            _logger.LogDebug($"{DateTime.Now.ToString("HH:mm:ss:fffff")} IncomingController HttpPost CreateFromText set date={model.Date}");
+
+            // количество // 60,000.00
+            //string rawPieces = textSplitted[startPointer + 5]; 
+            string pieces = textSplitted[startPointer + 5].Split('.').First();
+            model.Pieces = Int32.Parse(pieces.Replace(",", ""));
+            _logger.LogDebug($"{DateTime.Now.ToString("HH:mm:ss:fffff")} IncomingController HttpPost CreateFromText set Pieces={model.Pieces}");
+
+            // цена // 4,583.5 или 0.08708
+            //string rawPrice = textSplitted[startPointer + 6];
+            model.AvPrice = CleanString(textSplitted[startPointer + 6]);
+            _logger.LogDebug($"{DateTime.Now.ToString("HH:mm:ss:fffff")} IncomingController HttpPost CreateFromText set AvPrice={model.AvPrice}");
+
+            //комиссия = комиссия биржи + комиссия брокера 1.79	0.24
+            //string rawMoexComiss = textSplitted[startPointer + 11];
+            //string rawBrokComiss = textSplitted[startPointer + 12];
+            string moexComiss = CleanString(textSplitted[startPointer + 11]);
+            string brokComiss = CleanString(textSplitted[startPointer + 12]);
+            model.Comission = (decimal.Parse(moexComiss) + decimal.Parse(brokComiss)).ToString();
+            _logger.LogDebug($"{DateTime.Now.ToString("HH:mm:ss:fffff")} IncomingController HttpPost CreateFromText set Comission={model.Comission} from {moexComiss} + {brokComiss}");
+
+            // nkd // 289.20
+            //string rawNkd = textSplitted[startPointer + 9];
+            string nkd = CleanString(textSplitted[startPointer + 9]);
+            if (!nkd.Equals("0,00"))
+            {
+                model.NKD = nkd;
+            }
+            _logger.LogDebug($"{DateTime.Now.ToString("HH:mm:ss:fffff")} IncomingController HttpPost CreateFromText set NKD={model.NKD}");
+
+            // тикер
+            string rawIsin = textSplitted[startPointer + 3];
+            string rawSecCode = await _secCodesRepo.GetSecCodeByISIN(textSplitted[startPointer + 3]);
+            //проверить, что нам прислали действительно seccode а не ошибку
+            if (!StaticData.SecCodes.Any(x => x.SecCode == rawSecCode))// если нет
+            {
+                //отправим найденное в ошибки
+                ViewData["Message"] = model.SecCode;
+                model.SecCode = "0";//сбросим присвоенную ошибку
+            }
+
+            return View("Create", model);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(CreateDealsModel model)
@@ -165,6 +262,21 @@ namespace InvestmentVisualisation.Controllers
             }
 
             return RedirectToAction("Deals");
+        }
+
+
+
+        private string CleanString(string str)
+        {
+            if (str.EndsWith('.')) // убрать последнюю точку, чтобы RegEx не ругался
+            {
+                str = str.Substring(0, str.Length - 1);
+            }
+            str = str.Replace(",", "");
+
+            str = str.Replace(".", ",");
+
+            return str;
         }
     }
 }
