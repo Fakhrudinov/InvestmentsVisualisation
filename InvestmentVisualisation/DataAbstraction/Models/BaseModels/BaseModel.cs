287115aaa7b7f25442f8ff40cc0ebdbecee9445c
asbuka1975 <asbuka@gmail.com>
2023-02-25 10:10:41 +0300

    fix - comission remove bad validation

--- InvestmentVisualisation/DataAbstraction/Models/BaseModels/BaseModel.cs
+++ InvestmentVisualisation/DataAbstraction/Models/BaseModels/BaseModel.cs
@@ -8,7 +8,6 @@ namespace DataAbstraction.Models.BaseModels
         [Display(Name = "Дата")]
         public DateTime Date { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "The value must be greater than 0")]
         [Display(Name = "Комиссия")]
         [RegularExpression("^-?(0|[1-9]\\d{0,7})([.|,]\\d{1,6})?$", ErrorMessage = "Только цифры. Разделитель - точка или запятая.")]
         public string? Comission { get; set; }
