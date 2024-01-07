9f9d9252f75aa91923057a80658ff2655cb9da0d
asbuka1975 <asbuka@gmail.com>
2024-01-07 12:39:39 +0300

    done with wish

--- InvestmentVisualisation/InvestmentVisualisation/Controllers/WishListController.cs
+++ InvestmentVisualisation/InvestmentVisualisation/Controllers/WishListController.cs
@@ -1,4 +1,6 @@
 ﻿using DataAbstraction.Interfaces;
+using DataAbstraction.Models;
+using DataAbstraction.Models.BaseModels;
 using DataAbstraction.Models.WishList;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,6 +22,18 @@ namespace InvestmentVisualisation.Controllers
             _logger.LogInformation($"{DateTime.Now.ToString("HH:mm:ss:fffff")} WishListController WishList called");
 
             List<WishListItemModel> result = await _repository.GetFullWishList();
+
+            //create seccodes list for new wishes - remove all already used seccodes
+            List<StaticSecCode> secCodesList = new List<StaticSecCode>(StaticData.SecCodes
+                .GetRange(1, StaticData.SecCodes.Count - 1));//1=SUR, skip it
+            for (int i = secCodesList.Count - 1; i >= 0; i--)
+            {
+                if (result.Find(x => x.SecCode == secCodesList[i].SecCode) is not null)
+                {
+                    secCodesList.RemoveAt(i);
+                }
+            }
+            ViewBag.WishList = secCodesList;
 
             return View(result);
         }
