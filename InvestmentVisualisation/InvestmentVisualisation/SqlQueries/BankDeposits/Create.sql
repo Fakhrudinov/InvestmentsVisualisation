a0dc427164202a05fde9e9623bfc9abbf7db5337
asbuka1975 <asbuka@gmail.com>
2025-06-23 18:13:02 +0300

    unify db date column as event_date - fix sql script

--- InvestmentVisualisation/InvestmentVisualisation/SqlQueries/BankDeposits/Create.sql
+++ InvestmentVisualisation/InvestmentVisualisation/SqlQueries/BankDeposits/Create.sql
@@ -1,4 +1,4 @@
 ﻿INSERT INTO bank_deposits 
-	(`isopen`, `date_open`, `date_close`, `name`, `placed_name`, `percent`, `summ`) 
+	(`isopen`, `event_date`, `date_close`, `name`, `placed_name`, `percent`, `summ`) 
 VALUES 
 	(1, @date_open, @date_close, @name, @placed_name, @percent, @summ);
