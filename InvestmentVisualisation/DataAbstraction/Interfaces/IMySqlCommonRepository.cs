1dfd9a501659b5d535fb37edd1dfcb78973224e1
asbuka1975 <asbuka@gmail.com>
2025-12-15 22:37:38 +0300

    add recomended buy value

--- InvestmentVisualisation/DataAbstraction/Interfaces/IMySqlCommonRepository.cs
+++ InvestmentVisualisation/DataAbstraction/Interfaces/IMySqlCommonRepository.cs
@@ -1,4 +1,5 @@
-﻿namespace DataAbstraction.Interfaces
+﻿
+namespace DataAbstraction.Interfaces
 {
     public interface IMySqlCommonRepository
     {
@@ -7,6 +8,7 @@
         void FillStaticCategories();
         void FillStaticSecBoards();
         void FillStaticSecCodes();
+		string GetQueryParamsFromListOfStrigs(List<string> listOfSeccodes);
 		string? GetQueryTextByFolderAndFilename(string v1, string v2);
 		Task<int> GetTableCountBySqlQuery(CancellationToken cancellationToken, string query);
     }
